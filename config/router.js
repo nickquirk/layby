a538a2858c447990c8defd22bc78db05fc6fa31b
ajxrh <archibald.rh@gmail.com>
2022-11-17 14:07:17 +0000

    merge

--- config/router.js
+++ config/router.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import express from 'express'
-import { registerUser } from './controllers/auth.js'
+import { registerUser } from '../controllers/auth.js'
 
 const router = express.Router()
 
