b45859e7ca055e0fdfebbd6c54a6c4c5443cd704
Lucy Heath <lucyeheath@outlook.com>
2022-11-17 13:33:49 +0000

    started kiteSpot-controller

--- config/router.js
+++ config/router.js
@@ -7,16 +7,18 @@ const router = express.Router()
 
 // router.route('/regions/:id').get(getSingleRegion)
 
-// router.route('/regions/:id/location/:locationId').get(getLocation)
+// router.route('/regions/:id/locations').get(getAllLocations)
 
-router.route('/register').post(registerUser)
+// router.route('/regions/:id/locations/:locationId').get(getSingleLocation)
+
+// router.route('/register').post(registerUser)
 
 // router.route('/login').post(loginUser)
 
-// router.route('/location/:locationId/review').post(secureRoute, addReview)
+// router.route('/locations/:locationId/review').post(secureRoute, addReview)
 
 // router
-//   .route('/location/:locationId/review/:reviewId')
+//   .route('/locations/:locationId/review/:reviewId')
 //   .delete(secureRoute, deleteReview)
 //   .put(secureRoute, editReview)
 
