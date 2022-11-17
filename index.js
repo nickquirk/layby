2ad5f91510589d6c8ffdbadee15c10c1107a9245
Lucy Heath <lucyeheath@outlook.com>
2022-11-17 10:39:24 +0000

    router

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import {} from 'dotenv/config'
+import router from './config/router.js'
 
 const app = express()
 
 const serverStart = async () => {
   try {
     await mongoose.connect(process.env.DB_URI)
-    app.listen(process.env.PORT, () => console.log(`Server started on port => ${process.env.PORT}`))
+    app.listen(process.env.PORT, () =>
+      console.log(`Server started on port => ${process.env.PORT}`)
+    )
+    // * Router
+    app.use(router)
   } catch (err) {
     console.log('Server launch failed')
   }
