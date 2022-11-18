559f0151cc2d40cda49a2beb2f249aa69678dd4e
Nick Quirk <n.quirk87@gmail.com>
2022-11-18 15:08:33 +0000

    linking user register to backend

--- client/src/components/pages/RegisterPage.js
+++ client/src/components/pages/RegisterPage.js
@@ -1,17 +1,32 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const RegisterPage = () => {
 
+  // ! Location Variables
+  const navigate = useNavigate()
+
   // ! State 
   // Track state of following variables
-  const [ username, setUsername ] = useState('')
-  const [ email, setEmail] = useState('')
-  const [ password, setPassword ] = useState('')
+  const [ formFields, setFormFields ] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+  })
 
   // ! Executions 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
+    try {
+      await axios.post('/register')
+      // add navigate function
+      navigate('/login')
+    } catch (err) {
+      console.log(err)
+    }
+    console.log('form submitted')
   }
 
   const handleChange = (e) => {
@@ -23,11 +38,38 @@ const RegisterPage = () => {
       <h1>Register Page</h1>
       <div className='form-container'>
         <form onSubmit={handleSubmit}>
-          <input required className='form-control' type="text" name="username" onChange={handleChange} placeholder="Username"/>
-          <input required className='form-control' type="email" name="email" onChange={handleChange} placeholder="Email"/>
-          <input required className='form-control' type="password" name="password" onChange={handleChange} placeholder="Password"/>
-          <input required className='form-control' type="password" name="passwordConfirmation" onChange={handleChange} placeholder="Confirm password"/>
-          <Link to={'/login'} className='btn btn-main'>Submit</Link>
+          <input required 
+            className='form-control' 
+            type="text" 
+            name="username" 
+            onChange={handleChange} 
+            placeholder="Username"
+          />
+          <input 
+            required
+            className='form-control' 
+            type="email" 
+            name="email" 
+            onChange={handleChange} 
+            placeholder="Email"
+          />
+          <input 
+            required
+            className='form-control' 
+            type="password" 
+            name="password" 
+            onChange={handleChange} 
+            placeholder="Password"
+          />
+          <input 
+            required
+            className='form-control' 
+            type="password" 
+            name="passwordConfirmation" 
+            onChange={handleChange} 
+            placeholder="Confirm password"
+          />
+          <button className='btn btn-main'>Submit</button>
         </form>
       </div>
     </div>
