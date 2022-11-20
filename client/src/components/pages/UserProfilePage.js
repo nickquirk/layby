0e8730f00215989aa71e7a91cee024c15588af67
Nick Quirk <n.quirk87@gmail.com>
2022-11-20 09:37:16 +0000

    updated profile page

--- client/src/components/pages/UserProfilePage.js
+++ client/src/components/pages/UserProfilePage.js
@@ -17,11 +17,33 @@ const UserProfilePage = () => {
             <Card.Body>
               <Card.Img variant='top' src='https://tinyurl.com/5atpj5f8'/>
               <Card.Title>Location Name</Card.Title>
+              <Card.Subtitle>Country here</Card.Subtitle>
               <Card.Text>Review text here...</Card.Text>
+              <Card.Link>Link to review</Card.Link>
             </Card.Body>
           </Card>
         </div>  
         <div className='user-favourites'>
+          <h3>Favourite Places</h3>
+          <div className='favourite-card-container'>
+            <Card className='favourite-card'>
+              <Card.Body>
+                <Card.Img variant='top' src='https://tinyurl.com/5atpj5f8'/>
+                <Card.Title>Location Name</Card.Title>
+                <Card.Subtitle>Country here</Card.Subtitle>
+                <Card.Link>Link to location</Card.Link>
+              </Card.Body>
+            </Card>
+            <Card className='favourite-card'>
+              <Card.Body>
+                <Card.Img variant='top' src='https://tinyurl.com/5atpj5f8'/>
+                <Card.Title>Location Name</Card.Title>
+                <Card.Subtitle>Country here</Card.Subtitle>
+                <Card.Link>Link to location</Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+          
         </div>
       </div>
     </div>
