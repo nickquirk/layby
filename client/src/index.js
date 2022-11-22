5b529eba31f8bd4e7074154f7d5a19275a52111a
ajxrh <archibald.rh@gmail.com>
2022-11-22 23:17:06 +0000

    map/review half-working

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'mapbox-gl/dist/mapbox-gl.css'
 import './styles/main.scss'
 import App from './App'
 
