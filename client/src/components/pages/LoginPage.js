2c539fae6ed9269ee1082446ff383b24b4cd48a3
Lucy Heath <lucyeheath@outlook.com>
2022-11-25 16:03:51 +0000

    whatever

--- client/src/components/pages/LoginPage.js
+++ client/src/components/pages/LoginPage.js
@@ -2,7 +2,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-// imports
 import axios from 'axios'
 import { setToken } from '../common/Auth'
 
--- client/src/components/pages/NotFound.js
+++ client/src/components/pages/NotFound.js
@@ -8,5 +8,4 @@ const NotFound = () => {
     
   )
 }
-
 export default NotFound
