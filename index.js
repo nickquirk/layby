d60ad2a739b8e18ccda85504e520fab52dc62697
Lucy Heath <lucyeheath@outlook.com>
2022-11-25 14:15:50 +0000

    commiting

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-unused-vars */
 import express from 'express'
 import mongoose from 'mongoose'
 import { } from 'dotenv/config'
 import router from './config/router.js'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // ! Variables 
 const app = express()
@@ -25,6 +31,14 @@ const startServer = async () => {
     })
     // Router
     app.use('/api', router)
+
+    // ** New lines **
+    app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+
 
     // * Catch-All
     app.use((_req, res) => res.status(404).json({ message: 'Route not found' }))
